af4eb188f26e5922a8fddada9b21eb92b95ea071
jairedm1996 <jairedmobley@gmail.com>
2025-04-21 08:41:22 -0500

    My first rust program

---
+++ hello_world/target/debug/deps/hello_world-dce3d5db486bd0af.d
@@ -0,0 +1,5 @@
+/Users/jairedmobley/Desktop/rust-programming/hello_world/target/debug/deps/hello_world-dce3d5db486bd0af: src/main.rs
+
+/Users/jairedmobley/Desktop/rust-programming/hello_world/target/debug/deps/hello_world-dce3d5db486bd0af.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/jairedmobley/Desktop/rust-programming/hello_world/target/debug/hello_world: /Users/jairedmobley/Desktop/rust-programming/hello_world/src/main.rs
